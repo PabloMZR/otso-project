33c638aab29752703ea07f8bc5d90d5d5aea45d9
juan pablo mendoza <juantrane104@gmail.com>
2025-02-25 23:07:27 -0600

    Primer commit

---
+++ src/employees/employees.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEmployeeDto } from './dto/create-employee.dto';
+import { UpdateEmployeeDto } from './dto/update-employee.dto';
+
+@Injectable()
+export class EmployeesService {
+  private employees: CreateEmployeeDto[] = [
+    {
+    id: 1,
+    name: "Alberto",
+    lastName: "Costas",
+    phoneNumber: "123456789"
+    },
+    {
+    id: 2,
+    name: "Jose",
+    lastName: "Casas",
+    phoneNumber: "213456789"
+    },
+    {
+    id: 3,
+    name: "Juan",
+    lastName: "Perez",
+    phoneNumber: "312456789"
+    }
+]
+  create(createEmployeeDto: CreateEmployeeDto) {
+    createEmployeeDto.id = this.employees.length + 1;
+    this.employees.push(createEmployeeDto);
+    return createEmployeeDto;
+  }
+
+  findAll() {
+    //hacer que retorne todos los empleados
+    return this.employees;
+
+  }
+
+    findOne(id: number) {
+    const employee = this.employees.filter((employee) => employee.id === id)[0];
+    return employee;
+  }
+
+  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+    let employeeToUpdate = this.findOne(id);
+    employeeToUpdate = {
+      ... employeeToUpdate,
+      ... updateEmployeeDto
+    }
+    this.employees = this.employees.map((employee) => {
+      if (employee.id === id) {
+      }
+      return employee;
+    })
+    return employeeToUpdate;
+  }
+  remove(id: number) {
+    this.employees = this.employees.filter((employee) => employee.id !== id);
+  return this.employees;    
+  }
+}
