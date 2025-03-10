bd6c0f61dd2946d4e55cc79ed31d8e05555c413c
juan pablo mendoza <juantrane104@gmail.com>
2025-03-09 19:39:45 -0600

    error prueba docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmployeesModule } from './employees/employees.module';
 import { ProductsModule } from './products/products.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+console.log('DB_HOST:', process.env.host);
+console.log('DB_PORT:', process.env.port);
+console.log('DB_NAME:', process.env.name);
+console.log('DB_PASS:', process.env.pass);
 
 @Module({
-  imports: [EmployeesModule, ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: 'postgres',
+      password: "kakita420",
+      database: "otso_db",
+      entities: [],
+      synchronize: true,
+    }), EmployeesModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
